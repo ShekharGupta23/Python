cff7d88f3865ce73e7f208bc7d7c6ad829f0776a
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-03-26 21:54:21 +0530

    Add files via upload

---
+++ Odd_Even.py
@@ -0,0 +1,7 @@
+# Input seat number
+seat_number = int(input("Enter the seat number: "))
+
+if (seat_number % 2 == 0):
+    print("The seat number is even.")
+else:
+    print("The seat number is odd.")
