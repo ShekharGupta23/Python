eebf7a81d805f950131a0a4cfb01a4fc5a16e0de
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-03-27 22:23:42 +0530

    Add files via upload

---
+++ square_perimeter.py
@@ -0,0 +1,10 @@
+# A civil engineer needs to time a square floor.
+# Write a python program to calculate its perimeter and area using the side length.
+
+side_length = float(input("Enter the side length of the square: "))
+
+area = side_length * side_length
+perimeter = 4 * side_length
+
+print("Area of the square:", area)
+print("Perimeter of the square:", perimeter)
