79408dc39a5a5ccf3bfc9548d757f7f60483e79e
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-04-23 22:33:35 +0530

    Add files via upload

---
+++ MatrixPrograms/AddAndSubMatrices.py
@@ -0,0 +1,10 @@
+import numpy as np
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[4, 5], [6, 7]])
+
+print("Matrix A:\n", A)
+print("Matrix B:\n", B)
+
+# Adding matrices
+C = np.add(A, B)
+print("Result:\n", C)
