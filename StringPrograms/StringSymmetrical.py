294884811aefc4331737327d53d1413ae69795b3
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-04-25 14:06:29 +0530

    Add files via upload

---
+++ StringPrograms/StringSymmetrical.py
@@ -0,0 +1,8 @@
+s = "abaaba"
+
+half = len(s) // 2
+sym = s[:half] == s[half:] if len(s) % 2 == 0 else s[:half] == s[half+1:]
+
+pal = s == s[::-1]
+print("Symmetrical" if sym else "Not Symmetrical")
+print("Palindrome" if pal else "Not Palindrome")
