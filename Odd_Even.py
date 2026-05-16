ef5514c59c030bd480f749b77671d7c00162d06d
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-05-16 22:09:45 +0530

    Delete Odd_Even.py

--- Odd_Even.py
+++
@@ -1,7 +0,0 @@
-# Input seat number
-seat_number = int(input("Enter the seat number: "))
-
-if (seat_number % 2 == 0):
-    print("The seat number is even.")
-else:
-    print("The seat number is odd.")
