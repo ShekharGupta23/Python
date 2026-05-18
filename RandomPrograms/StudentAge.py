bea37283b0e266a74c02a39e1f84bd59440c2bfb
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-05-18 19:41:55 +0530

    Add files via upload

---
+++ RandomPrograms/StudentAge.py
@@ -0,0 +1,7 @@
+# Python program to check if a student is a teenager
+age = int(input("Enter the student's age: "))
+
+if (13 <= age <= 19):
+    print("The student is a teenager.")
+else:
+    print("The student is not a teenager.")
