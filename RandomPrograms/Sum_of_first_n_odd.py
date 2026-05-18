cce48431a60abaae1308ff33f367ad5d73a5dc14
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-05-18 19:46:03 +0530

    Add files via upload

---
+++ RandomPrograms/Sum_of_first_n_odd.py
@@ -0,0 +1,7 @@
+# A math teacher wants to demonstrate the sum of first n odd numbers. Write a python code to calculate it.
+
+n = int(input("Enter the number of odd numbers to sum: "))
+
+sum_of_odd_numbers = n ** 2
+
+print(f"The sum of the first {n} odd numbers is: {sum_of_odd_numbers}")
