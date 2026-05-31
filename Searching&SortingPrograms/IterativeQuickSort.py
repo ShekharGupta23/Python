222fc990b69f2a5f174fc4205f02307378878188
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-05-31 22:58:26 +0530

    Add files via upload

---
+++ Searching&SortingPrograms/IterativeQuickSort.py
@@ -0,0 +1,11 @@
+def partition(arr, l, h):
+    i = l - 1
+    pivot = arr[h] 
+
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
